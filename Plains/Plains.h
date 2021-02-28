b5aeeb3329cf73e140ae18069b79d3e2b1422080
Wilson Styres <wstyres@gmail.com>
2021-02-27 21:49:16 -0500

    Add project

---
+++ Plains/Plains.h
@@ -0,0 +1,18 @@
+//
+//  Plains.h
+//  Plains
+//
+//  Created by Wilson Styres on 2/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Plains.
+FOUNDATION_EXPORT double PlainsVersionNumber;
+
+//! Project version string for Plains.
+FOUNDATION_EXPORT const unsigned char PlainsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plains/PublicHeader.h>
+
+
