890d35ad722780958b9cc063ae84c98ea50f7819
Wilson Styres <wstyres@gmail.com>
2021-04-29 17:55:46 -0400

    Don't rely on my user directory for tests

--- PlainsTests/PlainsTests.mm
+++ PlainsTests/PlainsTests.mm
@@ -19,24 +19,28 @@
 @implementation PlainsTests
 
 - (void)setUp {
-    mkdir("/Users/wstyres/Library/Caches/xyz.willy.Plains", 0755);
-    mkdir("/Users/wstyres/Library/Caches/xyz.willy.Plains/logs", 0755);
-    mkdir("/Users/wstyres/Library/Caches/xyz.willy.Plains/lists", 0755);
+    NSString *cacheDir = [NSString stringWithFormat:@"%@/Library/Caches/xyz.willy.Plains", NSHomeDirectory()];
+    NSString *logDir = [NSString stringWithFormat:@"%@/logs", cacheDir];
+    NSString *listDir = [NSString stringWithFormat:@"%@/lists", cacheDir];
+    mkdir(cacheDir.UTF8String, 0755);
+    mkdir(logDir.UTF8String, 0755);
+    mkdir(listDir.UTF8String, 0755);
     
     config = [PLConfig sharedInstance];
     
     [config setBoolean:YES forKey:@"Acquire::AllowInsecureRepositories"];
-    [config setString:@"/Users/wstyres/Library/Caches/xyz.willy.Plains/logs" forKey:@"Dir::Log"];
-    [config setString:@"/Users/wstyres/Library/Caches/xyz.willy.Plains/lists" forKey:@"Dir::State::Lists"];
-    [config setString:@"/Users/wstyres/Library/Caches/xyz.willy.Plains/" forKey:@"Dir::Cache"];
-    [config setString:@"/Users/wstyres/Library/Caches/xyz.willy.Plains/" forKey:@"Dir::State"];
+    [config setString:logDir forKey:@"Dir::Log"];
+    [config setString:listDir forKey:@"Dir::State::Lists"];
+    [config setString:cacheDir forKey:@"Dir::Cache"];
+    [config setString:cacheDir forKey:@"Dir::State"];
     [config setString:@"/opt/procursus/libexec/zebra/supersling" forKey:@"Dir::Bin::dpkg"];
     [config setString:@"/opt/procursus/libexec/zebra/supersling" forKey:@"Plains::Slingshot"];
-    [config setString:@"/Users/wstyres/Library/Caches/xyz.willy.Plains/plains.sources" forKey:@"Plains::SourcesList"];
+    [config setString:[cacheDir stringByAppendingPathComponent:@"plains.sources"] forKey:@"Plains::SourcesList"];
 }
 
 - (void)tearDown {
-    [[NSFileManager defaultManager] removeItemAtPath:@"/Users/wstyres/Library/Caches/xyz.willy.Plains/" error:nil];
+    NSString *cacheDir = [config stringForKey:@"Dir::Cache"];
+    [[NSFileManager defaultManager] removeItemAtPath:cacheDir error:nil];
 }
 
 - (void)testAddSource {
